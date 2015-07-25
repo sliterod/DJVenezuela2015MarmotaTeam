d2faa73751fa72f88f325fbf39c438783a34ea70
adsith <carlosalvarez1512@gmail.com>
2015-07-26 01:20:33 +0200

    Funcion de Prallax
    Para lograr el efecto de paralaje en el cielo y eventualmente en las gradas.

---
+++ Assets/Scripts/Prallax.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prallax : MonoBehaviour {
+
+    public float INITIAL_POINT;
+    public float FINAL_POINT;
+    public float speed;
+
+    Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+        FINAL_POINT = Mathf.Abs(FINAL_POINT);
+        newPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if( Mathf.Abs(transform.localPosition.x) < FINAL_POINT)
+        {
+            newPosition.x = transform.localPosition.x - speed * Time.deltaTime;
+            transform.localPosition = newPosition;
+        }       	        
+	}
+}
